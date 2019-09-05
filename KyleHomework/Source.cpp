727f8626bfbc74a6883dd2f465d5fe8a8fd8c5fc
Jeremy Farmer <you.icaruspsy@gmail.com>
2019-09-05 19:31:04 -0400

    Updated with working program

---
+++ KyleHomework/Source.cpp
@@ -0,0 +1,29 @@
+/*
+Student: Jeremy Farmer
+Class: CSC 234-201
+Assignment: Program #3
+Purpose: Program with class files that have functions to read a binary log file
+*/
+
+#include<iostream>
+
+
+using namespace std;
+
+
+
+void PrintReport();
+
+int main() {
+	//Prints the report
+	PrintReport();
+	
+	cout << "Please work" << endl;
+	//Pause to view results
+	system("pause");
+	return 0;
+
+
+
+
+}
